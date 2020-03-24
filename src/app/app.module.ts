29a6d8df4d97b7d0df1202d18e725116a4ff4c8b
unknown <nitinjain.0786@gmail.com>
2020-03-24 17:27:33 +0530

    added rental list and detail components and service n modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes,RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { RentalModule } from './rental/rental.module';
+
+const routes :Routes =[
+{path:'',redirectTo: '/rentals' , pathMatch :'full'}
+]
 
 @NgModule({
   declarations: [
@@ -11,7 +17,9 @@ import {HeaderComponent} from './common/header/header.component';
     HeaderComponent
   ],
   imports: [
+  RouterModule.forRoot(routes),
     BrowserModule,
+    RentalModule,
     AppRoutingModule
   ],
   providers: [],
