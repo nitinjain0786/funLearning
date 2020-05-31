5616fceff500bd418e4572e23a661e490574cf96
unknown <nitinjain.0786@gmail.com>
2020-05-31 12:16:23 +0530

    hello world message

--- src/app/rental/rental-secret/rental-secret.component.ts
+++ src/app/rental/rental-secret/rental-secret.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalSecretComponent implements OnInit {
 
+   message = 'Hello World!';
+
   constructor() { }
 
   ngOnInit(): void {
